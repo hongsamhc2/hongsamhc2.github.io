0ed50eae0c38bde0668a5d28ed0f55af8897d9da
hongsamhc2 <hongsamgcp@gmail.com>
2022-04-27 15:46:05 +0900

    restart project

--- python/django_test/django_test/apps/polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from apps.polls.models import Question,Choice
-
-admin.site.register(Question)
-admin.site.register(Choice)
--- python/flask_upload/Flask_Admin/server.py
+++
@@ -1,6 +0,0 @@
-import routing
-app = routing.FlaskApp.creating_app()
-
-
-if __name__ =="__main__":
-    app.run(debug=True)
